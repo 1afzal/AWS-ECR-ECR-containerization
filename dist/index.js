784aa659e6b8c606a1423b934fc30d1e37fc7162
1afzal <afzalali5256@gmail.com>
2026-03-08 23:28:15 +0530

    dotenv bug fix

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import axios from "axios";
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT;
 app.get('/health', (req, res) => {
     return res.status(200).json({
