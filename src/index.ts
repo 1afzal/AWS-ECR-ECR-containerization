0db8f847e060ce55d38a9668e50b9981a7dde6db
1afzal <afzalali5256@gmail.com>
2026-03-08 22:09:45 +0530

    container orchestration

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT
+
+app.get('/health', (req,res)=>{
+    return res.status(200).json({
+        message: "Server live", 
+        status: "healthy!"
+    })
+})
+
+app.get('/info', async(req,res)=>{
+    try{
+        const result = await axios.get("https://jsonplaceholder.typicode.com/todos")
+        const data = result.data;
+        return res.status(200).json({
+            data: data
+        })
+    }
+    catch(err){
+        if(err instanceof Error){
+            console.log(err.message);
+            return res.status(400).json({
+                error: err.message
+            })
+        }
+    }
+
+
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server live at ${PORT}`)
+})
